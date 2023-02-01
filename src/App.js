511ccf2bd7a44af8d30ea27b9627d88d4b8e159a
cindyhu2023 <cindyhu2023@u.northwestern.edu>
2023-02-01 12:19:24 -0800

    fix the background image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './styles.css';
 import Landing from './landing';
 
-let path = process.env.PUBLIC_URL + '/image/';
-
 function App() {
   return (
     <div>
       <Landing/>
-      <img src={path + 'img1.jpg'} alt="bg1"/>
     </div>
   );
 }
